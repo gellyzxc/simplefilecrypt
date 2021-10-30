30bd613f0c5e1b74cf35fd283cbe4956f35e07b4
Yura Safin <urasaftn@gmail.com>
2021-10-31 01:17:45 +0500

    release

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import sys
 import random
 
+print(f'made by gellyzxc | tg:gekksume')
+
 ARRAY_SYMBOLS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
